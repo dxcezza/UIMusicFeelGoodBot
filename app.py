867d2d06df0b01af6b09ea4f10481dce235df74d
dxcezza <48048129+dxcezza@users.noreply.github.com>
2025-03-26 21:45:46 +0300

    Update app.py

--- app.py
+++ app.py
@@ -24,6 +24,7 @@ DATABASE_URL = os.getenv('DATABASE_URL')
 if not DATABASE_URL:
     raise ValueError("DATABASE_URL не установлен в переменных окружения")
 
+ytmusic = YTMusic()
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(bind=engine)
 
