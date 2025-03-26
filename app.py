050929ad4cdbc9d38eab676bca0fbd9139ccccf4
dxcezza <48048129+dxcezza@users.noreply.github.com>
2025-03-26 22:00:25 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, jsonify, send_file, send_from_directory
+from flask import Flask, request, jsonify, send_file, send_from_directory, Response
 import os
 import logging
 import io
